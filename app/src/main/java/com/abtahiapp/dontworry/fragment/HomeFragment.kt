0f581036564de2bbd0176fa3418dc9e7320c81ae
abtaaahi <abtahimahib@gmail.com>
2024-08-30 14:32:23 +0600

    HomeFragment-Concise

--- app/src/main/java/com/abtahiapp/dontworry/fragment/HomeFragment.kt
+++ app/src/main/java/com/abtahiapp/dontworry/fragment/HomeFragment.kt
@@ -8,7 +8,6 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.abtahiapp.dontworry.adapter.HomeAdapter
-import android.widget.Toast
 import com.abtahiapp.dontworry.GoogleCustomSearchResponse
 import com.abtahiapp.dontworry.HomeItem
 import com.abtahiapp.dontworry.HomeItemType
@@ -40,203 +39,118 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_home, container, false)
-        val homeRecyclerView: RecyclerView = view.findViewById(R.id.home_recycler_view)
-
-        homeRecyclerView.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
-        homeAdapter = HomeAdapter(requireContext(), mutableListOf())
-        homeRecyclerView.adapter = homeAdapter
-
+        setupRecyclerView(view)
         account = activity?.intent?.getParcelableExtra("account") ?: return view
-
         database = FirebaseDatabase.getInstance().getReference("user_information")
-
         fetchItems()
-
         return view
     }
 
+    private fun setupRecyclerView(view: View) {
+        val homeRecyclerView: RecyclerView = view.findViewById(R.id.home_recycler_view)
+        homeRecyclerView.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+        homeAdapter = HomeAdapter(requireContext(), mutableListOf())
+        homeRecyclerView.adapter = homeAdapter
+    }
+
     private fun fetchItems() {
-        fetchVideos { videos ->
-            fetchArticles { articles ->
-                fetchAudios{ audios ->
+        val currentDate = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(Date())
+        val moodHistoryRef = database.child(account.id!!).child("mood_history")
+        moodHistoryRef.orderByChild("date").equalTo(currentDate)
+            .addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    val lastMood = dataSnapshot.children.lastOrNull()?.child("mood")?.getValue(String::class.java)
+                    val queryType = when (lastMood) {
+                        "Angry" -> Triple("stress management", "relaxing music", "stress management")
+                        "Very Sad", "Sad" -> Triple("mental health", "soothing music", "mental health")
+                        "Fine", "Very Fine" -> Triple("positive thinking", "cheerful music", "positive thinking")
+                        else -> Triple("mindfulness", "calmness music", "mindfulness")
+                    }
+                    fetchData(queryType)
+                }
 
-                    val allItems = mutableListOf<HomeItem>()
+                override fun onCancelled(databaseError: DatabaseError) {
+                    updateUI(emptyList())
+                }
+            })
+    }
 
-                    allItems.addAll(videos)
-                    allItems.addAll(articles)
-                    allItems.addAll(audios)
+    private fun fetchData(queryType: Triple<String, String, String>) {
+        val (videoQuery, audioQuery, articleQuery) = queryType
 
-                    allItems.shuffle()
-
-                    homeAdapter.updateItems(allItems)
+        fetchVideos(videoQuery) { videos ->
+            fetchAudios(audioQuery) { audios ->
+                fetchArticles(articleQuery) { articles ->
+                    val allItems = mutableListOf<HomeItem>().apply {
+                        addAll(videos)
+                        addAll(audios)
+                        addAll(articles)
+                        shuffle()
+                    }
+                    updateUI(allItems)
                 }
             }
         }
     }
 
-    private fun fetchVideos(callback: (List<HomeItem>) -> Unit) {
-        val currentDate = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(Date())
-        val moodHistoryRef = database.child(account.id!!).child("mood_history")
+    private fun fetchVideos(query: String, callback: (List<HomeItem>) -> Unit) {
+        RetrofitClient.youtubeInstance.getVideos(query = query, apiKey = Secret.GOOGLE_API_KEY)
+            .enqueue(createResponseCallback(HomeItemType.VIDEO, callback))
+    }
 
-        moodHistoryRef.orderByChild("date").equalTo(currentDate)
-            .addListenerForSingleValueEvent(object : ValueEventListener {
-                override fun onDataChange(dataSnapshot: DataSnapshot) {
-                    val lastMood = if (dataSnapshot.exists()) {
-                        dataSnapshot.children.last().child("mood").getValue(String::class.java)
-                    } else {
-                        null
-                    }
+    private fun fetchAudios(query: String, callback: (List<HomeItem>) -> Unit) {
+        RetrofitClient.youtubeInstance.getVideos(query = query, apiKey = Secret.GOOGLE_API_KEY)
+            .enqueue(createResponseCallback(HomeItemType.AUDIO, callback))
+    }
 
-
-                    val apiKey = Secret.GOOGLE_API_KEY
-                    val query = when (lastMood) {
-                        "Angry" -> "stress management"
-                        "Very Sad", "Sad" -> "mental health"
-                        "Fine", "Very Fine" -> "positive thinking"
-                        else -> "mindfulness"
-                    }
-
-
-                    RetrofitClient.youtubeInstance.getVideos(query = query, apiKey = apiKey)
-                        .enqueue(object : Callback<VideoResponse> {
-                            override fun onResponse(call: Call<VideoResponse>, response: Response<VideoResponse>) {
-                                if (response.isSuccessful) {
-                                    val videoItems = response.body()?.items ?: emptyList()
-
-                                    val videoList = videoItems.map { video ->
-                                        HomeItem(
-                                            id = video.id.videoId,
-                                            title = video.snippet.title,
-                                            description = null,
-                                            imageUrl = video.snippet.thumbnails.high.url,
-                                            type = HomeItemType.VIDEO
-                                        )
-                                    }
-                                    callback(videoList)
-                                } else {
-                                    callback(emptyList())
-                                }
-                            }
-
-                            override fun onFailure(call: Call<VideoResponse>, t: Throwable) {
-                                callback(emptyList())
-                            }
-                        })
+    private fun fetchArticles(query: String, callback: (List<HomeItem>) -> Unit) {
+        RetrofitClient.instance.getSearchResults(query, Secret.CUSTOM_SEARCH_ENGINE_ID, Secret.GOOGLE_API_KEY)
+            .enqueue(object : Callback<GoogleCustomSearchResponse> {
+                override fun onResponse(call: Call<GoogleCustomSearchResponse>, response: Response<GoogleCustomSearchResponse>) {
+                    if (response.isSuccessful) {
+                        val articles = response.body()?.items?.map { item ->
+                            HomeItem(
+                                id = item.link,
+                                title = item.title,
+                                description = item.snippet,
+                                imageUrl = item.pagemap?.cse_image?.firstOrNull()?.src ?: "",
+                                type = HomeItemType.ARTICLE
+                            )
+                        } ?: emptyList()
+                        callback(articles)
+                    } else callback(emptyList())
                 }
 
-                override fun onCancelled(databaseError: DatabaseError) {
+                override fun onFailure(call: Call<GoogleCustomSearchResponse>, t: Throwable) {
                     callback(emptyList())
                 }
             })
     }
 
-    private fun fetchAudios(callback: (List<HomeItem>) -> Unit) {
-        val currentDate = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(Date())
-        val moodHistoryRef = database.child(account.id!!).child("mood_history")
+    private fun createResponseCallback(type: HomeItemType, callback: (List<HomeItem>) -> Unit): Callback<VideoResponse> {
+        return object : Callback<VideoResponse> {
+            override fun onResponse(call: Call<VideoResponse>, response: Response<VideoResponse>) {
+                if (response.isSuccessful) {
+                    val items = response.body()?.items?.map { video ->
+                        HomeItem(
+                            id = video.id.videoId,
+                            title = video.snippet.title,
+                            description = null,
+                            imageUrl = video.snippet.thumbnails.high.url,
+                            type = type
+                        )
+                    } ?: emptyList()
+                    callback(items)
+                } else callback(emptyList())
+            }
 
-        moodHistoryRef.orderByChild("date").equalTo(currentDate)
-            .addListenerForSingleValueEvent(object : ValueEventListener {
-                override fun onDataChange(dataSnapshot: DataSnapshot) {
-                    val lastMood = if (dataSnapshot.exists()) {
-                        dataSnapshot.children.last().child("mood").getValue(String::class.java)
-                    } else {
-                        null
-                    }
-
-                    val apiKey = Secret.GOOGLE_API_KEY
-                    val query = when (lastMood) {
-                        "Angry" -> "relaxing music"
-                        "Very Sad", "Sad" -> "soothing music"
-                        "Fine", "Very Fine" -> "cheerful music"
-                        else -> "calmness music"
-                    }
-
-                    RetrofitClient.youtubeInstance.getVideos(query = query, apiKey = apiKey)
-                        .enqueue(object : Callback<VideoResponse> {
-                            override fun onResponse(call: Call<VideoResponse>, response: Response<VideoResponse>) {
-                                if (response.isSuccessful) {
-                                    val videoItems = response.body()?.items ?: emptyList()
-
-                                    val videoList = videoItems.map { video ->
-                                        HomeItem(
-                                            id = video.id.videoId,
-                                            title = video.snippet.title,
-                                            description = null,
-                                            imageUrl = video.snippet.thumbnails.high.url,
-                                            type = HomeItemType.AUDIO
-                                        )
-                                    }
-                                    callback(videoList)
-                                } else {
-                                    callback(emptyList())
-                                }
-                            }
-
-                            override fun onFailure(call: Call<VideoResponse>, t: Throwable) {
-                                callback(emptyList())
-                            }
-                        })
-                }
-
-                override fun onCancelled(databaseError: DatabaseError) {
-                    callback(emptyList())
-                }
-            })
+            override fun onFailure(call: Call<VideoResponse>, t: Throwable) {
+                callback(emptyList())
+            }
+        }
     }
 
-    private fun fetchArticles(callback: (List<HomeItem>) -> Unit) {
-        val apiKey = Secret.GOOGLE_API_KEY
-        val customSearchEngineId = "f462f9035eadd418f"
-        val currentDate = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(Date())
-        val moodHistoryRef = database.child(account.id!!).child("mood_history")
-
-        moodHistoryRef.orderByChild("date").equalTo(currentDate)
-            .addListenerForSingleValueEvent(object : ValueEventListener {
-                override fun onDataChange(dataSnapshot: DataSnapshot) {
-                    val lastMood = if (dataSnapshot.exists()) {
-                        dataSnapshot.children.last().child("mood").getValue(String::class.java)
-                    } else {
-                        null
-                    }
-
-                    val query = when (lastMood) {
-                        "Angry" -> "stress management"
-                        "Very Sad", "Sad" -> "mental health"
-                        "Fine", "Very Fine" -> "positive thinking"
-                        else -> "mindfulness"
-                    }
-
-                    RetrofitClient.instance.getSearchResults(query, customSearchEngineId, apiKey)
-                        .enqueue(object : Callback<GoogleCustomSearchResponse> {
-                            override fun onResponse(call: Call<GoogleCustomSearchResponse>, response: Response<GoogleCustomSearchResponse>) {
-                                if (response.isSuccessful) {
-                                    val articleItems = response.body()?.items ?: emptyList()
-                                    val articleList = articleItems.map { item ->
-                                        HomeItem(
-                                            id = item.link,
-                                            title = item.title,
-                                            description = item.snippet,
-                                            imageUrl = item.pagemap?.cse_image?.firstOrNull()?.src ?: "",
-                                            type = HomeItemType.ARTICLE
-                                        )
-                                    }
-                                    callback(articleList)
-                                } else {
-                                    callback(emptyList())
-                                }
-                            }
-
-                            override fun onFailure(call: Call<GoogleCustomSearchResponse>, t: Throwable) {
-                                callback(emptyList())
-                                Toast.makeText(requireContext(), "Failed to fetch articles", Toast.LENGTH_SHORT).show()
-                            }
-                        })
-                }
-
-                override fun onCancelled(databaseError: DatabaseError) {
-                    Toast.makeText(requireContext(), "Failed to check mood history", Toast.LENGTH_SHORT).show()
-                    callback(emptyList())
-                }
-            })
+    private fun updateUI(items: List<HomeItem>) {
+        homeAdapter.updateItems(items)
     }
 }
